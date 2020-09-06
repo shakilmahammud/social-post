05d9e85169a8cbe5b8b18629718942261982a4ea
shakilmahammud <hshakil987@gmail.com>
2020-09-07 01:52:00 +0600

    social post react route metarial ui use project done

--- src/Component/PostComment/PostComment.js
+++ src/Component/PostComment/PostComment.js
@@ -5,32 +5,40 @@ import './PostComment.css';
 
 const PostComment = (props) => {
     const {postId}=useParams();
-   const [comment,setComment]=useState({});
-   const {name,email,body}=comment;
-   const {thumbnailUrl}=props.image;
+   const [comment,setComment]=useState([]);
     /* single post comment api */
     useEffect(()=>{
-        const url=`https://jsonplaceholder.typicode.com/comments/${postId}`;
+        const url=`https://jsonplaceholder.typicode.com/comments?postId=${postId}`;
         fetch(url)
         .then(res=>res.json())
         .then(data=>setComment(data))
     },[]);
     return (
         <>
-        <Container className="comment-textArea">
+        <h2>Comment:</h2>
+        {comment.map((comment) => (
+     <Container className="comment-textArea">
         <Grid container spacing={3}>
         <Grid item xs={1}>
-        <div className="commentArea"><img src={thumbnailUrl} alt=""/></div>
+        <img
+                  style={{ borderRadius: "50%", padding: "10px" }}
+                  src={`https://loremflickr.com/600/400?random=${comment.id}`}
+                  alt=""
+                  height="50"
+                  width="50"
+                />
+                
         </Grid>
         <Grid item xs={10}>
-         <div className="comment-text">
-         <h3> User Name : {name} </h3>
-         <p> User Email : {email} </p>
-         <p> Comment : <br/> {body} </p>
-         </div>
+        <div> 
+        <h3 style={{ padding: "10px" }}>User:{comment.name.slice(0, 10)}</h3>
+            <p>Email: {comment.email}</p>
+            <p>{comment.body}</p>
+        </div>
         </Grid>
         </Grid>
         </Container>
+          ))}
         </>
     );
 };
